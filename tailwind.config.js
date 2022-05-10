702d0ee34bfa4618471205ba204d5c7b148f578b
asadulR <asad1252001@gmail.com.com>
2022-05-10 09:44:34 +0600

    navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,20 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0FCFEC",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "",
+      "cupcake",
+    ],
+  },
   plugins: [require("daisyui")],
 }
